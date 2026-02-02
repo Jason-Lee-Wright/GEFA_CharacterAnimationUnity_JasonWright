0a258a4e3818735a71984316f861e0ac2a4356a3
Jason <jason974448@gmail.com>
2026-02-02 02:23:16 -0400

    Added a way to quit game, and did PP

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+}
